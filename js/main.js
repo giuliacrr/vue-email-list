c92bf6edc481c9cb9c4d1c4c5dd8da8927d68966
giuliacrr <giulia.corrado02@gmail.com>
2023-08-04 11:31:57 +0200

    complete + bonus

--- js/main.js
+++ js/main.js
@@ -5,13 +5,18 @@ Vue.createApp({
   data() {
     return {
       randomEmail: [],
+      emailCounter: 0,
     };
   },
   methods: {
     generateEmailBtn() {
-      axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((generEm) => {
-        this.randomEmail.push(generEm.data.response);
-      })
+      //Empty the array to print just 10 emails instead of adding them to previous ones
+      this.randomEmail = [];
+      for (let i = 0; i < 10; i++) {
+        axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((generEm) => {
+          this.randomEmail.push(generEm.data.response);
+        })
+      }
     }
   },
   mounted() {
